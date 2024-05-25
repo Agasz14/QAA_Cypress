0d659b42f68fc3b19c2fb5769a283bc47366757e
Agasz14 <146726350+Agasz14@users.noreply.github.com>
2024-05-25 16:26:58 +0200

    zadanie 4 - correct

--- cypress/e2e/HTTP_API.cy.js
+++ cypress/e2e/HTTP_API.cy.js
@@ -1,6 +1,3 @@
-import { assert } from "console";
-import { type } from "os";
-
 describe("Modul 4 - zadanie domowe - http tests", () => {
     it("response code should be 200", () => {
         cy.request('https://httpbin.org').then(response => {
@@ -19,18 +16,19 @@ it("response code should be 200", () => {
     cy.request('POST','https://httpbin.org/post').then(response => {
         const status = response.status;
         assert.equal(200, status);
+    })
 })
-})
+
 it("test duration", () => {
     cy.request('POST','https://httpbin.org/post').then(response => {
         assert.isTrue(response.duration <= 200);
-})
+    })
 })
 
 it("content-length check", () => {
     cy.request('GET','https://httpbin.org/image/png').then(response => {
-        assert.equal(8090, response.content-length);
-})
+        assert.equal(8090, response.headers['content-length']);
+    })
 })
 
 it("response code should be 200", () => {
@@ -41,27 +39,38 @@ it("response code should be 200", () => {
 })   
 it("test header", () => {
     cy.request('GET','https://httpbin.org/get').then(response => {
-        assert.equal('key=value', response.headers['Set-Cookie']);
+        assert.equal('application/json', response.headers['content-type']);
 })
 })
 
 it("response code should be 405", () => {
-    cy.request('PUT','https://httpbin.org/post').then(response => {
-        const status = response.status;
-        assert.equal(405, status);
+    cy.request({
+        method: 'PUT',
+        url: 'https://httpbin.org/post',
+        failOnStatusCode: false
+    }).then(response => {
+        expect(response.status).to.eq(405);
 })
 })
 
 it("body should be url", () => {
-    cy.request('GET','https://httpbin.org/post', {"key": "value"}).then(response => {
-        expect(response.body).to.have.property('key', 'value')
+    cy.request({
+        method: 'POST',
+        url: 'https://httpbin.org/post',
+        "key": "value"
+    }).then(response => {
+        expect(response.body).to.have.property('url')
     })
 })
 
 
 it("response code should be 400", () => {
-    cy.request('PATCH', 'https://httpbin.org/status/undefined', {failOnStatusCode: false}).then(response => {
-        assert.equal(400, response.status);
+    cy.request({
+        method: 'PATCH', 
+        url: 'https://httpbin.org/status/undefined',
+        failOnStatusCode: false
+    }).then(response => {
+        expect(response.status).to.eq(400);
     })
 })
 }); 
